7d5df269f396bf9653edcc855135263382388f0d
Johnnathan Sossa <69727862+JohnnySos@users.noreply.github.com>
2021-09-14 14:28:31 -0400

    Update CountChange.cpp

--- C++/Algos/CountChange.cpp
+++ C++/Algos/CountChange.cpp
@@ -4,16 +4,9 @@ using namespace std;
 int main()
 {
     int money, dollar,change, quarters, dimes, nickles, pennies;
-    // int dollar = 0;
 
     cout << "Please enter the number of cents you want coins for: ";
     cin >> money;
-
-    // for (int dollar = 0; money > 0; dollar++ ){
-    //     if (money >= 100){
-    //     dollar++;
-    //     }
-    // }
 
     // find the dollar
     dollar = money / 100;
@@ -49,23 +42,19 @@ int main()
 return 0;
 }
 
-/* BELOW is a simplified version with the inverse input method of asking for what coins want to be added, then adding said number of currencies to a sum total. Will be adding solution with for loop later.
+/* BELOW is a simplified version with the inverse input method of asking for what coins want to be added, instead of a sum value, 
+then adding said number of currencies to a sum total. Will be adding solution with for loop later to equivelent to decimal output; However normal output works fine.
 
 
 // int main()
 // {
 //     int dollar, quarters, dimes, nickels, total_cents;
-//     dollar = 0;
 //     cout << "Enter the number of quarters:\n";
 //     cin >> quarters;
 //     cout << "Enter the number of dimes:\n";
 //     cin >> dimes;
 //     cout << "Enter the number of nickels:\n";
 //     cin >> nickels;
-
-//     if (total_cents >= 100){
-            dollar++
-} 
 
 //     total_cents = (quarters * 25) + (dimes * 10) + (nickels * 5);
 
