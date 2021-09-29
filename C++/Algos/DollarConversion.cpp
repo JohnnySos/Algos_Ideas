13b41aa1e361cfc05eadad427f6cdcebcde7bc67
Johnnathan Sossa <69727862+JohnnySos@users.noreply.github.com>
2021-09-28 21:20:54 -0400

    Update DollarConversion.cpp

--- C++/Algos/DollarConversion.cpp
+++ C++/Algos/DollarConversion.cpp
@@ -1,6 +1,4 @@
-/* ***NOTES***.....I will add validation in later for additional usability; However I noticed using switch does not give me the same recursion error as using 
-if and else statements and allows me to call main within the function, so for now it will be completed like this with a fun validation easter egg...please dont 
-be mad at my humor =).*/
+/* ***NOTES***.....I will add validation in later for additional usability; However I noticed using switch does not give me the same recursion error as using if and else statements and allows me to call main within the function, so for now it will be completed like this with a fun validation easter egg...please dont be mad at my humor =).*/
 #include <iostream>
 using namespace std;
 
@@ -23,37 +21,40 @@ int main(){
     cout<< "Your Value of "<< dollar << " Dollars to Euro is equivalent to " << convEuro << " Euros."<< endl;
     cout<< "Your Value of "<< dollar << " Dollars to Mexican Pesos is equivalent to " << convPeso << " Pesos."<< endl;
 
+    cout<<endl;
+        cout<<"If you would like to go again type in Y when prompted, if not type N "<<endl;
+        cout<<"Please enter your response: ";
+        cin>> response;
 
-    cout<<"If you would like to go again type in Y when prompted, if not type N "<<endl;
-    cout<<"Please enter your response: ";
-    cin>> response;
+        switch(response){
+            case 'Y':
+            cout<<endl;
+                cout<< "Heey you wanna go again? Sounds like fun lets do it! "<< endl;
+            cout<<endl;
+                return main();
+                break;
 
-    switch(response){
-        case 'Y':
-            cout<< "Heey you wanna go again? Sounds like fun lets do it! "<< endl;
-            return main();
-            break;
+            case 'N':
+            cout<<endl;
+                cout<<"Sad to see you leave =( come back soon!" << endl;
+            cout<<endl;
+                return 0;
+                break;
 
-        case 'N':
-            cout<<"Sad to see you leave =( come back soon!" << endl;
-
-        default:
-            cout<<"Why you disobeying, you are making me sad =(....You will now be stuck in an infinite loop for your sub ordinance. =) "<<endl;
+            default:
             cout<<endl;
-            cout<<endl;
-            for (int i = 0; i<15; i++){
-                cout<<"HA HA HA, suffer =)"<<endl;
-                i++;
-            }
-            cout<<endl;
-            cout<<endl;
-            cout<<"Just kidding you can leave. Have a good day....but listen to directions next time =)"<<endl;
-            break;
-    }
+                cout<<"Why you disobeying, you are making me sad =(....You will now be stuck in an infinite loop for your sub ordinance. =) "<<endl;
+                cout<<endl;
+                cout<<endl;
+                for (int i = 0; i<15; i++){
+                    cout<<"HA HA HA, suffer =)"<<endl;
+                    i++;
+                }
+                cout<<endl;
+                cout<<endl;
+                cout<<"Just kidding you can leave. Have a good day....but listen to directions next time =)"<<endl;
+                break;
+        }
 
 return 0;
 }
-
-
-
-    
